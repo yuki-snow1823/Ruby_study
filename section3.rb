030c508a8800c11ee1088f5e46de6481bab6b04a
yuki-snow1823 <magic.iroha@gmail.com>
2020-04-28 20:00:54 +0900

    [finish]section3

---
+++ section3.rb
@@ -0,0 +1,28 @@
+# Unixのgrepコマンドもどきを作る＋コマンドラインからのデータ入力を学習する
+# ARGVというものを使うと、コマンドラインから引数を渡すことができる
+puts "最初の引数：#{ARGV[0]}"
+
+name = ARGV[0]
+
+puts "こんにちは" + name
+
+# 例えばファイルを開いて中身を出力するプログラムも作ることができる
+pattern = Regexp.new(ARGV[0])
+filename = ARGV[1]
+
+file = File.open(filename)
+file.each do |line|
+  if pattern =~line #くっついているところに意味がある
+    print line
+  end
+end 
+file.close
+
+# Railsなどでよくでてきたメソッド
+def hello
+  puts "こんにちは"
+end
+
+# requireを使えば、他のファイルのメソッドも呼び出せる
+hello()
+
