ea506007523a7436532e90c657abf18d2ebb4440
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-03 08:46:10 +0900

    [finish]section9 演算子

--- section9.rb
+++ section9.rb
@@ -7,3 +7,23 @@ end
 
 # 上のプログラムは以下と同じ
 name = var || "Ruby"
+
+item = nil
+if ary
+  item = ary.first
+end
+
+# 同義
+item = ary && ary.first
+
+# 同義
+item = ary&.first
+
+# 条件 ? 式1 : 式2
+# 条件演算子という
+
+# 範囲演算子
+# 数列を作り出す
+(1..10).to_a
+
+# 演算子が複数出てきて自身のない場合は、()をつかう 
