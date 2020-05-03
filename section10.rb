ff9af55635fd2e7b5c4571fb0de0b1255cfd8b52
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-03 09:30:53 +0900

    [finish]section10

--- section10.rb
+++ section10.rb
@@ -1,7 +1,12 @@
 # 例外処理の書き方
 
-# begin
-  # 例外を発生させる可能性のある処理
-# rescue => exception
-  # 例外の処理
-# end
+begin
+  例外を発生させる可能性のある処理
+rescue => exception
+  例外の処理
+end
+
+ensure 例外関係なく毎回実行する処理
+sleep 秒　秒数を開ける retryと組み合わせるともう一回試すことができる
+
+# エラーも全てオブジェクト。Expectationクラスのサブクラス
