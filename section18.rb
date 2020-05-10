a994da26f506703139ce442cea8f3c7d7221d57b
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-10 10:13:09 +0900

    [finish]section18 Fire/Dirクラス

--- section18.rb
+++ section18.rb
@@ -46,3 +46,50 @@ dir.close
 
 # ワイルドカードなど。ファイル名を全て取得できる
 # p Dir.glob("*.rb")
+
+
+# ファイルの情報
+p File.stat("section17.rb").ctime #いろんなキーがある。
+
+
+# ファイルの許可モードの設定
+# 8進数表記で行っているから3ビットは4桁で表す
+File.chmod(0755,"section17.rb")
+
+# ライブラリの紹介
+# find そのディレクトリ以下のファイルのパスを渡す
+
+# テンポラリファイル＝一時的なファイル
+
+# 練習問題
+def print_libraries
+  $:.each do |path|
+    # パスがディレクトリならばtrue => 今回は、でなければ次
+    next unless FileTest.directory?(path)
+    Dir.open(path) do |dir|
+      dir.each do |name|
+        if name =~ /\.rb$/i
+          # このiが謎？
+          puts name
+        end
+      end
+    end
+  end
+end
+
+print_libraries
+
+require "find"
+
+def du(path)
+  result = 0
+  Find.find(path){|f|
+    if File.file?(f)
+      result += File.size(f)
+    end
+  }
+  printf("%d %s\n", result, path)
+  return result
+end
+
+du(ARGV[0] || ".")
