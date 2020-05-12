322310012fa0cb89131726811ae755d4d47a9727
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-12 10:49:20 +0900

    [add]section22 ファイルのDL/テキストの切り出し

---
+++ section22.rb
@@ -0,0 +1,16 @@
+# テキスト処理の学習
+# 青空文庫のテキストを使う
+
+require "open-uri"
+
+url = "https://www.aozora.gr.jp/cards/001779/files/56647_58167.html"
+filename = "nisendouka.html"
+
+# ダウンロードされる
+
+File.open(filename, "wb") do |f|
+  text = open(url, "r:shift_jis").read
+  f.write text.encode("utf-8")
+end
+
+	# <div class="main_text"><br /> が本海野スタート
