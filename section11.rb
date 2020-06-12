85b18fb19b8c4ce064e7644523940db5dcb5168c
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-12 10:08:21 +0900

    [review]section11 ブロック

--- section11.rb
+++ section11.rb
@@ -6,9 +6,9 @@ outcome = {"参加費" => 1000,"ストラップ代" => 500}
 outcome.each do |*pair|
   p pair
 end
-
 p sum
 
+# each_XXという種類が多い
 # *ブロック変数で結果を配列に入れてくれる
 
 # 引数に&をつけたものをproc引数といい、ブロックを入れることができる
@@ -23,5 +23,22 @@ end
 
 p [x,y]
 
+# arr.sort {|a,b| a.length<=>b.length}のように、ブロック変数と条件を書くことで、そのように並び替えることができる
 # xは初期値のままで更新されないが、yはxに最後に入った3となる。（xは初期化の１がそのまま入っている）
 # yの初期化をなくすとちなみにエラーを起こす。ブロック変数は外では呼べない
+
+
+# sort_byの方が処理が短くなる
+
+# def total(from,to)
+#   result = 0
+#   from.upto do |num|
+#     if block_given?
+#     else
+#   end
+# end
+
+# yieldを使うとブロック変数に引数渡せるのかも
+# next redo breakにはデフォルト値の設定ができる
+
+# Procを使うことで、ブロックをオブジェクトとして持ち運べる
